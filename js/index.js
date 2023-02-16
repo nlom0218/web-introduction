b300d6ceec05eb51b16f204103e312a54f2e8e8a
nlom0218 <nlom0218@naver.com>
2023-02-16 22:39:43 +0900

    feat: 이름과 취향 개수 띄우기

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import movies from './movies.js';
 
 const $movieList = document.querySelector('.movie-list');
 const $guestBook = document.querySelector('.guest-book');
+const $registerButton = document.querySelector('.register-button');
 
 function App() {
   this.init = () => {
@@ -28,10 +29,26 @@ function App() {
   const randerGuestBook = () => {
     $guestBook.innerHTML = movies
       .map((movie) => {
-        return `<label><input type="checkbox" name="taste" value=${movie.title}/>${movie.title}</label>`;
+        return `<label><input class="taste-input" type="checkbox" name="taste"/>${movie.title}</label>`;
       })
       .join('');
   };
+
+  const clickRegisterButton = () => {
+    const $tasteInput = document.querySelectorAll('.taste-input');
+    const $nameInput = document.querySelector('.name-input');
+
+    const name = $nameInput.value;
+    const checkedCount = [...$tasteInput].reduce((acc, cur) => {
+      if (cur.checked) return (acc += 1);
+      return acc;
+    }, 0);
+
+    if (!name) return;
+    window.alert(`${name}님, 저와 ${checkedCount}개의 취향이 같으시네요!`);
+  };
+
+  $registerButton.addEventListener('click', clickRegisterButton);
 }
 const app = new App();
 app.init();
